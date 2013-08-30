44e2396176dad03f512e0c22a846841b5349e330
Liber <gjl87910lq@hotmail.com>
2013-08-30 16:14:32 +0800

    office

--- lesson_b/recall.rb
+++ lesson_b/recall.rb
@@ -1,6 +1,9 @@
 ﻿require 'sinatra'
-require "sinatra/reloader" if development?
+require 'sinatra/reloader' if development?
 require 'data_mapper'
+
+SITE_TITLE = "Recall"  
+SITE_DESCRIPTION = "'cause you're too busy to remember"
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")  
   
@@ -15,9 +18,65 @@ end
   
 DataMapper.finalize.auto_upgrade!
 
+helpers do
+    include Rack::Utils  
+    alias_method :h, :escape_html  
+end
+
 ##
 get '/' do  
   @notes = Note.all :order => :id.desc  
   @title = 'All Notes'  
-  erb :home  
+  erb :home
 end
+
+post '/' do  
+  n = Note.new  
+  n.content = params[:content]  
+  n.created_at = Time.now  
+  n.updated_at = Time.now  
+  n.save  
+  redirect '/'  
+end
+
+get '/rss.xml' do
+  @notes = Note.all :order => :id.desc  
+  builder :rss
+end
+
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
+end
+
+put '/:id' do  
+  n = Note.get params[:id]  
+  n.content = params[:content]  
+  n.complete = params[:complete] ? 1 : 0  
+  n.updated_at = Time.now  
+  n.save  
+  redirect '/'  
+end
+
+get '/:id/delete' do  
+  @note = Note.get params[:id]  
+  @title = "Confirm deletion of note ##{params[:id]}"  
+  erb :delete  
+end
+
+delete '/:id' do  
+  n = Note.get params[:id]  
+  n.destroy  
+  redirect '/'  
+end
+
+get '/:id/complete' do  
+  n = Note.get params[:id]  
+  n.complete = n.complete ? 0 : 1 # flip it  
+  n.updated_at = Time.now  
+  n.save  
+  redirect '/'  
+end
+
+
